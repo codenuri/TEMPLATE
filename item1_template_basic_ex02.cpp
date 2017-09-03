53fab84ae7c18b59a9587318fc7da1686afaeeef
codenuri <smkang@codenuri.co.kr>
2017-09-03 22:58:44 +0900

    Add files via upload

---
+++ item1_template_basic_ex02.cpp
@@ -0,0 +1,23 @@
+/*
+* HOME       : www.codelec.co.kr
+* COURSENAME : C++ Template Programming
+* Copyright (C) 2017 CODENURI Inc. All rights reserved.
+*/
+
+// item1_template_basic_ex02.cpp
+
+#define MAKE_SQUARE(T)	\
+T square(T a)			\
+{						\
+	T ret = a * a;		\
+	return ret;			\
+}
+
+MAKE_SQUARE(int)
+MAKE_SQUARE(double)
+
+int main()
+{
+	square(3);
+	square(3.3);
+}
