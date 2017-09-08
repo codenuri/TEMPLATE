5aff36f213a0946918dcbfac694a227ab2e38ad3
codenuri <smkang@codenuri.co.kr>
2017-09-08 16:28:02 +0900

    Add files via upload

---
+++ item3_ex02.cpp
@@ -0,0 +1,22 @@
+﻿/*
+* HOME       : www.codelec.co.kr
+* COURSENAME : C++ Template Programming
+* Copyright (C) 2017 CODENURI Inc. All rights reserved.
+*/
+
+// item03_ex2.cpp
+
+template<typename T> void foo(T a)
+{
+	*a = 10;
+}
+
+int main()
+{
+//	if ( false )
+//		foo(0);
+
+	// C++17
+	if constexpr (false)
+		foo(0);
+}
