8a91d676ffc4a0a89bab847989556b9e0deafdfe
codenuri <smkang@codenuri.co.kr>
2017-10-01 20:21:11 +0900

    Add files via upload

---
+++ constexpr.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+template<int N> struct check {};
+
+// constexpr 함수 - C++11
+constexpr int add(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int n = add(1, 2);
+
+	check< add(1, 2) > c; // ok..
+
+
+	int n1 = 1, n2 = 2;
+
+	int c = add(n1, n2); // ok
+
+	//check< add(n1, n2) > c; // error
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ factorial.cpp
@@ -0,0 +1,37 @@
+﻿/*
+* HOME       : ecourse.co.kr
+* EMAIL      : smkang @ codenuri.co.kr
+* COURSENAME : C++ Template Programming
+* MODULE     : factorial.cpp
+* Copyright (C) 2017 CODENURI Inc. All rights reserved.
+*/
+
+#include <iostream>
+using namespace std;
+
+// template meta programming
+template<int N> struct factorial
+{
+	//int value = 10;
+	//enum { value = N * factorial<N-1>::value };
+	static constexpr int value = N * factorial<N - 1>::value;
+};
+// 재귀의 종료를 위해 특수화 문법 사용
+template<> struct factorial<1>
+{
+	//enum { value = 1 };
+	static constexpr int value = 1;
+};
+
+int main()
+{
+	int n = factorial<5>::value; // 5 * 4 * 3 * 2 * 1  => 120 
+	//			5 * f<4>::v
+	//				4 * f<3>::v
+	//					3 * f<2>::v
+	//						2 * f<1>::v
+	//							1	
+
+	cout << n << endl;
+}
+
